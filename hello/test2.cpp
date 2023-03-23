23ac32d25ced6abaa7cdcb9f4e6db72c5d98dee3
Ahsan Habib Rasel <jucse28.367@gmail.com>
2023-03-24 03:27:49 +0600

    hello world

---
+++ hello/test2.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+// cerr << " Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << " ms  \n " ;
+using namespace std;
+#define int long long
+#define pi acos(-1)
+#define all(x) x.begin(), x.end()
+#define endl "\n"
+#define deb(x) cout << #x << "=" << x << endl;
+#define debg cout << "yes" << endl;
+#define fast  ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define test int t; cin >> t;  while (t--)
+#define pb push_back
+#define vec vector<int>
+#define firs first
+#define sec second
+#define fo(i, n) for (int i = 0; i < n; i++)
+#define rep(i, a, n) for (int i = a; i <= n; i++)
+#define ln length()
+#define spc(x) cout << x << " ";
+#define ent(x) cout << x << "\n";
+#define mem(x, v) memset(x, v, sizeof(x));
+int power(int  n,int  m)
+{
+    int  ans=1;
+    while(m)
+    {
+        ans*=n;
+        m--;
+    }
+    return ans;
+}
+int32_t main()
+{
+   cout<<"hello world"<<endl;
+   cout<<"hello world"<<endl;
+   cout<<"hello world"<<endl;
+   cout<<"hello world"<<endl;
+
+}
